d39e1c1caba009d8818d1fa811d17390d822eda2
khoa-omega <nvkhoa05@gmail.com>
2022-09-10 22:34:18 +0700

    Upload slide-01-generator-uuid

---
+++ src/main/java/com/vti/entity/Student.java
@@ -0,0 +1,61 @@
+package com.vti.entity;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "name", length = 50, unique = true, nullable = false)
+    private String name;
+
+    @Column(name = "gender", nullable = false)
+    @Convert(converter = GenderConverter.class)
+    private Gender gender;
+
+    public Student() {
+    }
+
+    public Student(String name, Gender gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", gender=" + gender +
+                '}';
+    }
+}
