7ef971da9d21485b19cb47a34ed96975e42ba146
khoa-omega <nvkhoa05@gmail.com>
2022-09-12 21:09:09 +0700

    Update AccountDTO.java

--- src/main/java/com/vti/dto/AccountDTO.java
+++ src/main/java/com/vti/dto/AccountDTO.java
@@ -1,10 +1,12 @@
 package com.vti.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @RequiredArgsConstructor
 public class AccountDTO {
 	@NonNull
