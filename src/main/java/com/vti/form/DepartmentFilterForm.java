1413868d375bda4d982677fcb39ed79a3ab68ae8
khoa-omega <nvkhoa05@gmail.com>
2022-09-14 07:36:06 +0700

    Upload slide-08-specification-compare-date

--- src/main/java/com/vti/form/DepartmentFilterForm.java
+++ src/main/java/com/vti/form/DepartmentFilterForm.java
@@ -2,9 +2,23 @@ package com.vti.form;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
 public class DepartmentFilterForm {
     private String search;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate createdDate;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate minCreatedDate;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate maxCreatedDate;
+
+    private Integer minYear;
 }
--- src/main/java/com/vti/specification/DepartmentSpecification.java
+++ src/main/java/com/vti/specification/DepartmentSpecification.java
@@ -8,7 +8,9 @@ import com.vti.form.DepartmentFilterForm;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.util.StringUtils;
 
-import javax.persistence.criteria.*;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
+import java.time.LocalDate;
 
 public class DepartmentSpecification {
     public static Specification<Department> buildWhere(DepartmentFilterForm form) {
@@ -22,6 +24,45 @@ public class DepartmentSpecification {
             }
             Join<Department, Account> join = root.join(Department_.accounts, JoinType.LEFT);
             return builder.like(join.get(Account_.username), "%" + value.trim() + "%");
+        };
+    }
+
+    public static Specification<Department> hasCreatedDateEqual(LocalDate createdDate) {
+        return (root, query, builder) -> {
+            if (createdDate == null) {
+                return null;
+            }
+            return builder.equal(root.get(Department_.createdDate).as(LocalDate.class), createdDate);
+        };
+    }
+
+    public static Specification<Department> hasCreatedDateGreaterThanOrEqualTo(LocalDate minCreatedDate) {
+        return (root, query, builder) -> {
+            if (minCreatedDate == null) {
+                return null;
+            }
+            return builder.greaterThanOrEqualTo(root.get(Department_.createdDate).as(LocalDate.class), minCreatedDate);
+        };
+    }
+
+    public static Specification<Department> hasCreatedDateLessThanOrEqualTo(LocalDate maxCreatedDate) {
+        return (root, query, builder) -> {
+            if (maxCreatedDate == null) {
+                return null;
+            }
+            return builder.lessThanOrEqualTo(root.get(Department_.createdDate).as(LocalDate.class), maxCreatedDate);
+        };
+    }
+
+    public static Specification<Department> hasMinYearGreaterThanOrEqualTo(Integer minYear) {
+        return (root, query, builder) -> {
+            if (minYear == null) {
+                return null;
+            }
+            return builder.greaterThanOrEqualTo(
+                    builder.function("YEAR", Integer.class, root.get(Department_.createdDate)),
+                    minYear
+            );
         };
     }
 }
