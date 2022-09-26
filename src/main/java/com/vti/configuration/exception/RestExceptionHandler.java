1683064ac73a35f36355f91cc2717d084c9ddf89
khoa-omega <nvkhoa05@gmail.com>
2022-09-26 20:31:09 +0700

    Upload slide-10-global-exception-02

--- src/main/java/com/vti/configuration/exception/RestExceptionHandler.java
+++ src/main/java/com/vti/configuration/exception/RestExceptionHandler.java
@@ -3,11 +3,24 @@ package com.vti.configuration.exception;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
@@ -18,6 +31,7 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         return messageSource.getMessage(key, null, "Lỗi server", LocaleContextHolder.getLocale());
     }
 
+    // Default exception
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleException(Exception exception) {
         String message = getMessage("Exception.message");
@@ -27,5 +41,148 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
         ErrorResponse response = new ErrorResponse(message, detailMessage, null, code, moreInformation);
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    // Not found url handler
+    @Override
+    protected ResponseEntity<Object> handleNoHandlerFoundException(
+            NoHandlerFoundException exception,
+            HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        String message = getMessage("NoHandlerFoundException.message")
+                + exception.getHttpMethod() + " " + exception.getRequestURL();
+        String detailMessage = exception.getLocalizedMessage();
+        int code = 2;
+        String moreInformation = String.format("http://localhost:8080/api/v1/exception/%d", code);
+
+        ErrorResponse response = new ErrorResponse(message, detailMessage, null, code, moreInformation);
+        return new ResponseEntity<>(response, status);
+    }
+
+    private String getMessageFromHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException exception) {
+        StringBuilder message = new StringBuilder(exception.getMethod());
+        message.append(" ").append(getMessage("HttpRequestMethodNotSupportedException.message"));
+        for (HttpMethod method : exception.getSupportedHttpMethods()) {
+            message.append(method).append(" ");
+        }
+        return message.toString();
+    }
+
+    // Not support HTTP Method
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(
+            HttpRequestMethodNotSupportedException exception,
+            HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        String message = getMessageFromHttpRequestMethodNotSupportedException(exception);
+        String detailMessage = exception.getLocalizedMessage();
+        int code = 3;
+        String moreInformation = String.format("http://localhost:8080/api/v1/exception/%d", code);
+
+        ErrorResponse response = new ErrorResponse(message, detailMessage, null, code, moreInformation);
+        return new ResponseEntity<>(response, status);
+    }
+
+    private String getMessageFromHttpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException exception) {
+        StringBuilder message = new StringBuilder(exception.getContentType() + getMessage("HttpMediaTypeNotSupportedException.message"));
+        for (MediaType method : exception.getSupportedMediaTypes()) {
+            message.append(method).append(", ");
+        }
+        return message.substring(0, message.length() - 2);
+    }
+
+    // Not support media type
+    @Override
+    protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(
+            HttpMediaTypeNotSupportedException exception,
+            HttpHeaders headers, HttpStatus status,
+            WebRequest request
+    ) {
+        String message = getMessageFromHttpMediaTypeNotSupportedException(exception);
+        String detailMessage = exception.getLocalizedMessage();
+        int code = 4;
+        String moreInformation = "http://localhost:8080/api/v1/exception/4";
+
+        ErrorResponse response = new ErrorResponse(message, detailMessage, null, code, moreInformation);
+        return new ResponseEntity<>(response, status);
+    }
+
+    // BindException: This exception is thrown when fatal binding errors occur.
+    // MethodArgumentNotValidException: This exception is thrown when argument
+    // annotated with @Valid failed validation:
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException exception,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request
+    ) {
+
+        String message = getMessage("MethodArgumentNotValidException.message");
+        String detailMessage = exception.getLocalizedMessage();
+        // error
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError error : exception.getBindingResult().getFieldErrors()) {
+            String fieldName = error.getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        }
+        int code = 5;
+        String moreInformation = "http://localhost:8080/api/v1/exception/5";
+
+        ErrorResponse response = new ErrorResponse(message, detailMessage, errors, code, moreInformation);
+        return new ResponseEntity<>(response, status);
+    }
+
+    // bean validation error
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException exception) {
+        String message = getMessage("ConstraintViolationException.message");
+        String detailMessage = exception.getLocalizedMessage();
+        // error
+        Map<String, String> errors = new HashMap<>();
+        for (ConstraintViolation<?> violation : exception.getConstraintViolations()) {
+            String fieldName = violation.getPropertyPath().toString();
+            String errorMessage = violation.getMessage();
+            errors.put(fieldName, errorMessage);
+        }
+        int code = 5;
+        String moreInformation = "http://localhost:8080/api/v1/exception/5";
+
+        ErrorResponse response = new ErrorResponse(message, detailMessage, errors, code, moreInformation);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    // MissingServletRequestPartException: This exception is thrown when when the part of a multipart request not found
+    // MissingServletRequestParameterException: This exception is thrown when request missing parameter:
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(
+            MissingServletRequestParameterException exception,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request
+    ) {
+
+        String message = exception.getParameterName() + " " + getMessage("MissingServletRequestParameterException.message");
+        String detailMessage = exception.getLocalizedMessage();
+        int code = 6;
+        String moreInformation = "http://localhost:8080/api/v1/exception/6";
+
+        ErrorResponse response = new ErrorResponse(message, detailMessage, null, code, moreInformation);
+        return new ResponseEntity<>(response, status);
+    }
+
+    // TypeMismatchException: This exception is thrown when try to set bean property with wrong type.
+    // MethodArgumentTypeMismatchException: This exception is thrown when method argument is not the expected type:
+    @ExceptionHandler({ MethodArgumentTypeMismatchException.class })
+    public ResponseEntity<Object> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException exception) {
+        String message = exception.getName() + getMessage("MethodArgumentTypeMismatchException.message")
+                + exception.getRequiredType().getName();
+        String detailMessage = exception.getLocalizedMessage();
+        int code = 7;
+        String moreInformation = "http://localhost:8080/api/v1/exception/7";
+
+        ErrorResponse response = new ErrorResponse(message, detailMessage, null, code, moreInformation);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
