2f94d6be758242a7ccfade11681b94fbce005984
khoa-omega <nvkhoa05@gmail.com>
2023-02-19 12:16:13 +0700

    Update

--- src/main/java/com/vti/entity/Group.java
+++ src/main/java/com/vti/entity/Group.java
@@ -11,34 +11,14 @@ import javax.persistence.Table;
 @Table(name = "group")
 public class Group {
     @Id
-    @Column(name = "id")
+    @Column(name = "id", nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(name = "name", length = 50, unique = true, nullable = false)
     private String name;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
     public void setName(String name) {
         this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "Group{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
     }
 }
